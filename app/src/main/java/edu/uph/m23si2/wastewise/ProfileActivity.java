455808d6b99ba748783dae8ad2e734ec29db571d
arronkennedy <aktkenpro@gmail.com>
2025-07-12 04:22:08 +0700

    Push Progress kedua (Arron)

--- app/src/main/java/edu/uph/m23si2/wastewise/ProfileActivity.java
+++ app/src/main/java/edu/uph/m23si2/wastewise/ProfileActivity.java
@@ -1,14 +1,25 @@
 package edu.uph.m23si2.wastewise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
+    LinearLayout btnPersonalDetail, btnSettings, btnMyOutlet, btnContactUs, btnFAQs;
+
+    ImageView btnNotif, btnBack;
+
+    CardView crvPersonalDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,53 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+
+        btnPersonalDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toPersonalDetail();
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toSetting();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toProfile();
+            }
+        });
+    }
+
+    public void init(){
+        btnPersonalDetail = findViewById(R.id.btnPersonalDetail);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnMyOutlet = findViewById(R.id.btnMyOutlet);
+        btnContactUs = findViewById(R.id.btnContactUs);
+        btnFAQs = findViewById(R.id.btnFAQs);
+        btnNotif = findViewById(R.id.btnNotif);
+        btnBack = findViewById(R.id.btnBack);
+        crvPersonalDetail = findViewById(R.id.crvPersonalDetail1);
+    }
+
+    public void toPersonalDetail(){
+        Intent intent = new Intent(this, PersonalDetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void toSetting(){
+        Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+    }
+
+    public void toProfile(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/uph/m23si2/wastewise/adapter/ProductAdapter.java
@@ -0,0 +1,62 @@
+package edu.uph.m23si2.wastewise.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import edu.uph.m23si2.wastewise.R;
+import edu.uph.m23si2.wastewise.model.Product;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Product> productArrayList;
+
+    public ProductAdapter(Context context, ArrayList<Product> productArrayList) {
+        this.context = context;
+        this.productArrayList = productArrayList;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imvProduk;
+        TextView txvJumlahItem, txvAlamatOutlet, txvHarga;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imvProduk = itemView.findViewById(R.id.imvProduk);
+            txvJumlahItem = itemView.findViewById(R.id.txvJumlahItem);
+            txvAlamatOutlet = itemView.findViewById(R.id.txvAlamatOutlet);
+            txvHarga = itemView.findViewById(R.id.txvHarga);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_produk, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Product product = productArrayList.get(position);
+
+        holder.imvProduk.setImageResource(product.getLogoOutlet());
+        holder.txvJumlahItem.setText(product.getJumlahItem());
+        holder.txvAlamatOutlet.setText(product.getAlamat());
+        holder.txvHarga.setText("Rp " + product.getHarga());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productArrayList.size();
+    }
+}
