71748bdac97bd967aaf3d12dc90e92dcbcc7d770
Thomas Herpin <thomasherpin312@gmail.com>
2025-07-20 02:13:35 +0700

    profile penjual

---
+++ app/src/main/java/com/example/wastewise/LanguageSellerActivity.java
@@ -0,0 +1,45 @@
+package com.example.wastewise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LanguageSellerActivity extends AppCompatActivity {
+    ImageView btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_language_seller);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        init();
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toSettings();
+            }
+        });
+    }
+
+    public void init(){
+        btnBack = findViewById(R.id.btnBack);
+    }
+
+    public void toSettings(){
+        Intent intent = new Intent(this, SettingSellerActivity.class);
+        startActivity(intent);
+    }
+}
