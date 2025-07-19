8f7732176fc83972dbc0849e769a18272d649c5a
Thomas Herpin <thomasherpin312@gmail.com>
2025-07-19 23:01:24 +0700

    hapus action bar

--- app/src/main/java/com/example/wastewise/MainActivity.java
+++ app/src/main/java/com/example/wastewise/MainActivity.java
@@ -20,20 +20,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialToolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_product,
-                R.id.nav_activity, R.id.nav_account)
-                .build();
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
-
 }
